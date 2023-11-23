09e38f7ae3533fa58d04727bcf8a25067eeae146
Dimitri Klopfstein <dimitri.kft@gmail.com>
2023-11-23 11:35:35 +0100

    add icon link header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import styles from './Header.module.css'; // Assurez-vous que le chemin est correct
 import { useRouter } from 'next/router'; // Ajoutez cette ligne
+import { MinusSquare, CreditCard, Codesandbox, Box } from 'react-feather';
 
 const Header = () => {
   const router = useRouter();
@@ -18,16 +19,20 @@ const Header = () => {
           <Link href="/">
             <img src="/logoTpf.svg" alt="Logo" className={styles.logo} />
           </Link>
-          <Link href="/">
+          <Link href="/" className={styles['link-container']}>
+            <Box className={router.pathname === "/" ? styles['icon-active'] : styles.icon} />
             <p className={getLinkClassName("/")}>Accueil</p>
           </Link>
-          <Link href="/cours">
+          <Link href="/cours" className={styles['link-container']}>
+          <Codesandbox className={router.pathname === "/cours" ? styles['icon-active'] : styles.icon} />
             <p className={getLinkClassName("/cours")}>Cours</p>
           </Link>
-          <Link href="/tuto">
+          <Link href="/tuto" className={styles['link-container']}>
+          <MinusSquare className={router.pathname === "/tarif" ? styles['icon-active'] : styles.icon} />
             <p className={getLinkClassName("/tuto")}>Tutoriels</p>
           </Link>
-          <Link href="/traif">
+          <Link href="/tarif" className={styles['link-container']}>
+          <CreditCard className={router.pathname === "/tarif" ? styles['icon-active'] : styles.icon} />
             <p className={getLinkClassName("/tarif")}>Tarif</p>
           </Link>
         </nav>
