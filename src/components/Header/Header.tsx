022b138cca1c6fe84cd8a1fef86f22537a33bb32
Dimitri Klopfstein <dimitri.kft@gmail.com>
2023-11-23 16:58:17 +0100

    mise en place responsive menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,47 @@
-// Header.tsx
+// // Header.tsx
+
+
+
+import React, { useState } from 'react';
 import Link from 'next/link';
-import styles from './Header.module.css'; 
-import { useRouter } from 'next/router'; 
-import { MinusSquare, CreditCard, Codesandbox, Box } from 'react-feather';
+import styles from './Header.module.css';
+import { useRouter } from 'next/router';
+
+ import { MinusSquare, CreditCard, Codesandbox, Box, Menu } from 'react-feather'; // Importez l'icône Menu
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const router = useRouter();
-  4
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   const getLinkClassName = (path: string): string => {
     return router.pathname === path ? styles.activeLink : styles.link;
   };
 
- 
   return (
     <div className={styles.headerContainer}>
       <header className={styles.header}>
+        <div className={styles.responsiveMenu}>
+          <Link href="/">
+            <img src="/logoTpf.svg" alt="Logo" className={styles.logoResponsive} />
+          </Link>
+          <button className={styles.burgerMenu} onClick={toggleMenu}>
+            <Menu />
+          </button>
+        </div>
+
+        {/* Menu déroulant pour le mode responsive */}
+        {isMenuOpen && (
+          <nav className={styles.navResponsive}>
+            {/* Ajoutez ici vos liens pour le menu responsive */}
+            {/* Vous pouvez réutiliser getLinkClassName pour les liens */}
+          </nav>
+        )}
+
+        {/* Barre de navigation principale pour le mode desktop */}
         <nav className={styles.navbar}>
           <Link href="/">
             <img src="/logoTpf.svg" alt="Logo" className={styles.logo} />
@@ -24,15 +51,15 @@ const Header = () => {
             <p className={getLinkClassName("/")}>Accueil</p>
           </Link>
           <Link href="/cours" className={styles['link-container']}>
-          <Codesandbox className={router.pathname === "/cours" ? styles['icon-active'] : styles.icon} />
+            <Codesandbox className={router.pathname === "/cours" ? styles['icon-active'] : styles.icon} />
             <p className={getLinkClassName("/cours")}>Cours</p>
           </Link>
           <Link href="/tuto" className={styles['link-container']}>
-          <MinusSquare className={router.pathname === "/tarif" ? styles['icon-active'] : styles.icon} />
+            <MinusSquare className={router.pathname === "/tarif" ? styles['icon-active'] : styles.icon} />
             <p className={getLinkClassName("/tuto")}>Tutoriels</p>
           </Link>
           <Link href="/tarif" className={styles['link-container']}>
-          <CreditCard className={router.pathname === "/tarif" ? styles['icon-active'] : styles.icon} />
+            <CreditCard className={router.pathname === "/tarif" ? styles['icon-active'] : styles.icon} />
             <p className={getLinkClassName("/tarif")}>Tarif</p>
           </Link>
         </nav>
@@ -42,3 +69,9 @@ const Header = () => {
 };
 
 export default Header;
+
+
+
+
+
+
